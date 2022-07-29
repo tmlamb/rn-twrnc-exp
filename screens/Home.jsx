c14280d6f01a89b0b8bcb77483eceb47dcd897bb
Tom Lamb <tmlamb@gmail.com>
2022-07-29 11:05:38 -0500

    always render Home screen

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -5,14 +5,22 @@ Created: Sat Jan 15 2022 17:37:34 GMT+0530 (India Standard Time)
 Copyright (c) Geekofia 2022 and beyond
 */
 
-import { View, Button, Text } from "react-native";
+import { useFocusEffect } from '@react-navigation/native';
+import { useCallback, useState } from 'react';
+import { Button, Text, View } from "react-native";
 import tw from "twrnc";
 
 const Home = ({ navigation }) => {
   const handleNavigation = () => navigation.navigate("About");
+  const [value, setValue] = useState(0)
 
+  useFocusEffect(
+    useCallback(() => {
+      setValue(v => v + 1)
+    }, [])
+  )
   return (
-    <View style={tw`flex-1 dark:bg-gray-900`}>
+    <View key={value} style={tw`flex-1 dark:bg-gray-900`}>
       <Text
         style={tw`m-auto text-center text-black 
         dark:text-white`}
